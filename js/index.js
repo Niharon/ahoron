9211dbe1ad6e5d4088f6207d31106d56768a8e15
Abduz Zami <69592754+AbduzZami@users.noreply.github.com>
2022-06-08 01:19:05 +0600

    update

--- js/index.js
+++ js/index.js
@@ -47,3 +47,20 @@ document.getElementById("cat5").addEventListener("click", () => {
 document.getElementById("cat6").addEventListener("click", () => {
   catHandler(6);
 });
+
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
